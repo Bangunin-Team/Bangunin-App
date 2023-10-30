528c5b009667d54f9b9208cbe08831106c5d0e5a
Fauzi Arda <fauziardasaputra@gmail.com>
2023-10-30 17:59:27 +0700

    implement post detail page

---
+++ presentation/presentation/Example/PostDetail/PostDetailVM.swift
@@ -0,0 +1,8 @@
+//
+//  PostDetailVM.swift
+//  presentation
+//
+//  Created by Fauzi Arda on 30/10/23.
+//
+
+import Foundation
---
+++ presentation/presentation/Example/PostDetail/PostDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  PostDetailView.swift
+//  presentation
+//
+//  Created by Fauzi Arda on 30/10/23.
+//
+
+import Foundation
