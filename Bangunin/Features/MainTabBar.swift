40279b80310b0c82532dbef8d0e83967ff1166d8
Fauzi Arda <fauziardasaputra@gmail.com>
2023-11-07 18:45:02 +0700

    setup google maps api

--- Bangunin/Features/MainTabBar.swift
+++ Bangunin/Features/MainTabBar.swift
@@ -8,9 +8,29 @@
 import Foundation
 import presentation
 import UIKit
+import GoogleMaps
 
 struct MainTabBarUIComposer {
     static func makeTabBar() -> UITabBarController {
-        return MainTabBarViewController()
+        let controller = MainTabBarViewController()
+        
+        controller.makeHomeViewController = {
+            let homeController = HomeViewController()
+            let camera:GMSCameraPosition = GMSCameraPosition.camera(withLatitude: -6.230549777155396, longitude: 106.84466959353516, zoom: 15)
+            let mapView = GMSMapView(frame: .zero, camera: camera)
+            let mapDelegate = MapViewDelegateClass()
+            mapView.delegate = mapDelegate
+            
+            homeController.loadMap = {
+                return mapView
+            }
+            return homeController
+        }
+        
+        return controller
     }
+}
+
+class MapViewDelegateClass: NSObject, GMSMapViewDelegate {
+    
 }
